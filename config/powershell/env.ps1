5b51cf0de03f524d2cf65a86521c142af087688d
Ángel García Beltrán <angelurano00@gmail.com>
2026-06-28 01:46:22 +0200

    Add yazi settings for windows powershell

--- config/powershell/env.ps1
+++ config/powershell/env.ps1
@@ -5,3 +5,7 @@ $env:FZF_ALT_C_COMMAND = 'fd --type=d --hidden --strip-cwd-prefix --exclude .git
 $env:FZF_DEFAULT_OPTS = "--height 40% --layout=reverse --border"
 $env:FZF_CTRL_T_OPTS = "--height 40% --border --layout=reverse --preview 'bat --color=always -n --line-range :250 {}'"
 $env:FZF_ALT_C_OPTS = "--height 40% --border --layout=reverse --preview ''"
+
+# Yazi
+$env:YAZI_FILE_ONE = "$HOME\scoop\apps\git\current\usr\bin\file.exe"
+
